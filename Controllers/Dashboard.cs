500b6773bb49d985975756ef7206558b93c00936
Ricardo Martinez <ricko62291@gmail.com>
2020-09-09 11:12:54 -0500

    first commit

---
+++ Controllers/Dashboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adelante_Inventory_V1.Controllers
+{
+    public class Dashboard : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
